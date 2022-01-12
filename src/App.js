1ea96c9f635a786682ff504793ce439c0163e4a7
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-12 14:48:05 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import LogIn from "./components/Login";
+import UserProfile from "./components/UserProfile";
+import Debit from "./components/Debit";
+import Credit from "./components/Credit";
 
 function App() {
+
+
+
+
   return (
+    // Browser Router at the top so everything inside can reference it
+    // Links and components are in routes
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar>
+      <Router>
+        <div>
+          <Route exact path="/" element={HomeComponent} />
+          <Route exact path="/login" element={LogInComponent} />
+          <Route exact path="/userProfile" element={UserProfileComponent} />
+          <Route exact path="/debit" element={DebitComponent} />
+          <Route exact path="/credit" element={CreditComponent} />
+        </div>
+      </Router>
+      </NavBar>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
+function NavBar()
+{
+  return(
+    <div>
+        <ul>
+        <li><a Link to = "/" > Home Page</a></li>
+        <li><a Link to = "/login" > User Log In</a></li>
+        <li><a Link to = "/userProfile" > Your Profile</a></li>
+        <li><a Link to = "/debit" > Your Debit Card Information</a></li>
+        <li><a Link to = "/credit" > Your Credit Card Information</a></li>
+        </ul>
+
+
+
+    </div>
+  )
+
+}
 export default App;
